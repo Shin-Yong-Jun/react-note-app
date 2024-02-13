210957a7094b1fcd7c217027ee7772c2db68fd01
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-02-13 12:05:39 +0900

    상태 리덕스 store, reducer 구성 및 main.tsx에 반영

--- src/store/tags/tagsSlice.ts
+++ src/store/tags/tagsSlice.ts
@@ -1,1 +1,19 @@
- 
+import { createSlice } from "@reduxjs/toolkit";
+import {v4} from "uuid";
+
+const initialState = {
+  tagsList: [
+    {tag: "learnings", id: v4()},
+    {tag: "work", id: v4()},
+    {tag: "quotes", id: v4()},
+  ],
+
+}
+
+const tagsSlice = createSlice({
+  name: "tags",
+  initialState,
+  reducers: {},
+})
+
+export default tagsSlice.reducer;
